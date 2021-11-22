33ba42fde29809523e616e424dfc99c9dc9eb3f5
julieta <julietaagras@gmail.com>
2021-11-22 12:47:18 -0300

    INITIAL COMMIT

---
+++ src/main/java/com/BriCode/gestorsocios/GestorSociosApplication.java
@@ -0,0 +1,13 @@
+package com.BriCode.gestorsocios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorSociosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorSociosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BriCode/gestorsocios/GestorSociosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BriCode.gestorsocios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorSociosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
